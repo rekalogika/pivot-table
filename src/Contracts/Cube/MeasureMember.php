df4c6dd3300c254696be9e216399983d57d76b28
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-08-17 23:17:40 +0700

    chore: add interface comments

--- src/Contracts/Cube/MeasureMember.php
+++ src/Contracts/Cube/MeasureMember.php
@@ -13,9 +13,28 @@ declare(strict_types=1);
 
 namespace Rekalogika\PivotTable\Contracts\Cube;
 
+/**
+ * MeasureMember represents a member of a measure in a cube. A measure member
+ * will be the member of a dimension if that dimension represents a measure.
+ *
+ * A measure dimension is an instance of `Dimension` that will have these
+ * properties:
+ *
+ * * `getName()` always returns `@values`
+ * * `getLegend()` returns the name of the measure, e.g. 'Total Sales'
+ * * `getMember()` returns an instance of this interface
+ */
 interface MeasureMember
 {
+    /**
+     * Get the value of the measure member. This is the value of the measure for
+     * this specific member. Example: 'sumOfSales'
+     */
     public function getMeasureName(): string;
 
+    /**
+     * The legend of the measure member. This is a human-readable name for
+     * the measure member. Example: 'Total Sales'
+     */
     public function getLegend(): mixed;
 }
